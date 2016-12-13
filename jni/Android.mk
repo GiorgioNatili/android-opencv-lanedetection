5b69a629fa702e32734c4b723109ebc473af156d
XealRedan <alexandre.lombard@live.fr>
2016-12-13 21:43:41 +0100

    Conf changes

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,5 +1,8 @@
 LOCAL_PATH := $(call my-dir)
 
+# FIXME Do not use hard-coded SDK location here
+OPENCVSDK := C:/Users/Alexandre/Downloads/OpenCV-2.4.9-android-sdk/sdk
+
 include $(CLEAR_VARS)
 
 #OpenCv
@@ -10,7 +13,8 @@ include $(OPENCVSDK)/native/jni/OpenCV.mk
 
 LOCAL_MODULE    := LaneDetectionNative
 LOCAL_SRC_FILES := LaneDetector.cpp
-LOCAL_LDLIBS += -llog -ldl -landroid -lGLESv2 -lEGL 
+LOCAL_LDFLAGS += -Wl,--gc-sections
+LOCAL_LDLIBS += -llog -ldl -landroid -lGLESv2 -lEGL -latomic
 
 CPPFLAGS += -fno-strict-aliasing -mfpu=vfp -mfloat-abi=softfp
 LOCAL_CPP_FEATURES := rtti exceptions
--- jni/Application.mk
+++ jni/Application.mk
@@ -1,5 +1,5 @@
 APP_STL := gnustl_static
-APP_CPPFLAGS := -frtti -fexceptions
+APP_CPPFLAGS := -frtti -fexceptions -std=c++11
 APP_ABI := all
 APP_PLATFORM := android-18
 APP_OPTIM := release
